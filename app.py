f49587afdb7ce256999724cf03a1c70907f7feda
Arman <armanshaikh236058@gmail.com>
2025-04-16 22:51:40 +0530

    Appp

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def get_data():
         return jsonify({"status": "ignored", "message": "Empty keyboardData"}), 204
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0", port=5000)
